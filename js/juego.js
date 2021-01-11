32dc59db93d1c97822b47a8aa86490eebd9f8d03
miguicole <miguicole@gmail.com>
2021-01-11 20:51:15 +0100

    Add files via upload
    boceto juego 2

--- js/juego.js
+++ js/juego.js
@@ -8,17 +8,24 @@ function tiradaDados(){
 	for(var i = 1; i<=5;i++){
 		var dado = dadoRandom();
 		var img = $('<img />', { 
-  			class: dado,
+  			class: dado, //para saber el numero
   			src: ("img/" + (dado.toString()) + ".png")
 			});
-		img.addClass("dado");
+		img.addClass("dado");//para los estilos
 		img.appendTo($('#div2'));
 	}
 
 }
 $(document).ready(function() {
     $("#lanzadado").click(function(){
+    	$('#div2').empty();
     	tiradaDados();
+    });
+    $('body').on('click','img',function(){
+    	var dadoGuardado = this;
+    	//$('#div3').append("<img class='" + this.attr("class") + "' src='img/2.png'>");
+    	console.log(this.getClass());
+    	this.remove();
     });
 });
 //tiradaDados();
